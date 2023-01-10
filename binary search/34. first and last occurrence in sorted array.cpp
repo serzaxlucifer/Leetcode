cbf8c89d6177d803864cc560330237d69dea43d0
Mukul Malik <85161950+serzaxlucifer@users.noreply.github.com>
2023-01-10 18:37:49 +0530

    Update 34. first and last occurrence in sorted array.cpp

--- binary search/34. first and last occurrence in sorted array.cpp
+++ binary search/34. first and last occurrence in sorted array.cpp
@@ -39,3 +39,69 @@ vector<int> searchRange(vector<int>& nums, int target) {
 
 // other solution:
 
+vector<int> searchRange(vector<int>& nums, int target) {
+        int fo = -1;
+        int lo = -1;
+
+        int start = 0;
+        int end = nums.size()-1;
+        int mid = start + (end - start)/2;
+
+        // find first occurence!
+        while(start <= end)
+        {
+            if(nums[mid] == target)
+            {
+                if(mid == 0 || nums[mid-1] != nums[mid])
+                {
+                    fo = mid;
+                    break;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
+            }
+            else if(nums[mid] > target)
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+            mid = start + (end - start)/2;
+        }
+
+        start = 0;
+        end = nums.size()-1;
+        mid = start + (end - start)/2;
+
+        // find last occurence!
+        while(start <= end)
+        {
+            if(nums[mid] == target)
+            {
+                if(mid == nums.size()-1 || nums[mid+1] != nums[mid])
+                {
+                    lo = mid;
+                    break;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+            }
+            else if(nums[mid] > target)
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+            mid = start + (end - start)/2;
+        }
+
+        return {fo, lo};
+    }
