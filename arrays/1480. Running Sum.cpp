04ead421c590c40c6f6fde833848bc76c9d33254
Mukul Malik <85161950+serzaxlucifer@users.noreply.github.com>
2023-01-15 21:30:02 +0530

    Create 1480. Running Sum.cpp

---
+++ arrays/1480. Running Sum.cpp
@@ -0,0 +1,12 @@
+vector<int> runningSum(vector<int>& nums) 
+{
+
+    int size = nums.size();
+
+    for(int i = 1; i < size; i++)
+    {
+        nums[i] += nums[i-1];
+    }
+
+    return nums;
+}
