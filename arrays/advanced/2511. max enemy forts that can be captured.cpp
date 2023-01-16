fbab285a7a695cace863c55532b264ff4b32a943
Mukul Malik <85161950+serzaxlucifer@users.noreply.github.com>
2023-01-16 23:07:49 +0530

    Update 2511. max enemy forts that can be captured.cpp

--- arrays/advanced/2511. max enemy forts that can be captured.cpp
+++ arrays/advanced/2511. max enemy forts that can be captured.cpp
@@ -1,6 +1,6 @@
 // https://leetcode.com/problems/maximum-enemy-forts-that-can-be-captured/description/ 
 
-// 0ms RunTime Solution:
+// 0ms RunTime Solution: (Single Pointer and Single Traversal)
 int captureForts(vector<int>& forts) 
     {
         int maxCapture = 0;                     // if not possible to capture any base.
@@ -51,3 +51,102 @@ int captureForts(vector<int>& forts)
     }
 
 // another solution:
+    int captureForts(vector<int>& forts) 
+    {
+        int maxCapture = 0;
+        int basesCaptured = 0;
+
+        int capturing = 0;
+        int size = forts.size(); 
+
+        for(int i = 0; i < size; i++)
+        {
+            if(forts[i] == 1)
+            {
+                if(capturing == 2)
+                {
+                    maxCapture = (maxCapture >= basesCaptured) ? maxCapture : basesCaptured;
+                }
+                capturing = 1;
+                basesCaptured = 0;
+            }
+            else if(forts[i] == 0)
+            {
+                if(capturing != 0)
+                {
+                    basesCaptured++;
+                }
+            }
+            else
+            {
+                if(capturing == 1)
+                maxCapture = (maxCapture >= basesCaptured) ? maxCapture : basesCaptured;
+                capturing = 2;
+                basesCaptured = 0;
+            }
+        }
+
+        return maxCapture;
+    }
+
+// another solution:
+
+int captureForts(vector<int>& forts) 
+    {
+        int maxCapture = 0;
+        int deployPoint = 0;
+        int relocationPointIndex = -1;
+
+        bool capturing = false;
+        int size = forts.size(); 
+
+        for(int i = 0; i < size; i++)
+        {
+            if(forts[i] == 1)
+            {
+                relocationPointIndex = i;
+                if(capturing)
+                {
+                    maxCapture = (maxCapture >= i-deployPoint-1) ? maxCapture : i-deployPoint-1;
+                    deployPoint = 0;
+                }
+                capturing = false;
+            }
+            else if(forts[i] == -1)
+            {
+                if(relocationPointIndex != -1)
+                {
+                    maxCapture = (maxCapture >= i-relocationPointIndex-1) ? maxCapture : i-relocationPointIndex-1;
+                }
+                deployPoint = i;
+                relocationPointIndex = -1;
+                capturing = true;
+                
+            }
+        }
+
+        return maxCapture;
+    }
+
+// smallest code:
+
+int captureForts(vector<int>& forts) 
+    {
+        int maxCapture = 0;
+        int size = forts.size(); 
+        int pointOfInterest = 0;
+
+        for(int i = 0; i < size; i++)
+        {
+            if(forts[i] != 0)
+            {
+                if(forts[pointOfInterest] == -forts[i])
+                {
+                    maxCapture = (maxCapture >= (i-pointOfInterest-1)) ? maxCapture : (i-pointOfInterest-1); 
+                }
+                pointOfInterest = i;
+            }
+        }
+
+        return maxCapture;
+    }
