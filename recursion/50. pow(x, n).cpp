63aca896d12a2a26dcebb0a0792407a9fce26d01
Mukul Malik <85161950+serzaxlucifer@users.noreply.github.com>
2023-01-19 19:36:59 +0530

    Create 50. pow(x, n).cpp

---
+++ recursion/50. pow(x, n).cpp
@@ -0,0 +1,44 @@
+// 5 solutions:
+
+
+//nest myPow
+double myPow(double x, int n) {
+    if(n<0) return 1/x * myPow(1/x, -(n+1));
+    if(n==0) return 1;
+    if(n==2) return x*x;
+    if(n%2==0) return myPow( myPow(x, n/2), 2);
+    else return x*myPow( myPow(x, n/2), 2);
+}
+//2. double myPow
+double myPow(double x, int n) { 
+    if(n==0) return 1;
+    double t = myPow(x,n/2);
+    if(n%2) return n<0 ? 1/x*t*t : x*t*t;
+    else return t*t;
+}
+//3. double x
+double myPow(double x, int n) { 
+    if(n==0) return 1;
+    if(n<0){
+        n = -n;
+        x = 1/x;
+    }
+    return n%2==0 ? myPow(x*x, n/2) : x*myPow(x*x, n/2);
+}
+//4. iterative one
+double myPow(double x, int n) { 
+    if(n==0) return 1;
+    if(n<0) {
+        n = -n;
+        x = 1/x;
+    }
+    double ans = 1;
+    while(n>0){
+        if(n&1) ans *= x;
+        x *= x;
+        n >>= 1;
+    }
+    return ans;
+}
+
+// Code:
