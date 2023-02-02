2c2f050ff3b4113e9fed73ddb88eed49cd67c755
Mukul Malik <85161950+serzaxlucifer@users.noreply.github.com>
2023-02-03 01:39:41 +0530

    Create 1380. Lucky Number.cpp

---
+++ amazing optimizations/1380. Lucky Number.cpp
@@ -0,0 +1,33 @@
+// Only one lucky number will exist at max.
+
+vector<int> luckyNumbers (vector<vector<int>>& arr) {
+        int m, n;
+        m = arr.size();
+        n = arr[0].size();
+        
+        vector < int >ans;
+        for (int i = 0; i < m; ++i)
+            {
+            int minIdx = 0;
+            for (int j = 0; j < n; ++j)
+            {
+            if (arr[i][j] < arr[i][minIdx])
+                minIdx = j;
+            }
+
+            bool flag = true;
+            for (int k = 0; k < m; ++k)
+            {
+            if (arr[k][minIdx] > arr[i][minIdx])
+                {
+                flag = false;
+                break;
+                }
+            }
+
+            if (flag)
+            ans.push_back (arr[i][minIdx]);
+            }
+
+        return ans;
+    }
