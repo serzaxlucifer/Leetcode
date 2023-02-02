fde7eb248db789fb764e34bcb0c566d3db6f90af
Mukul Malik <85161950+serzaxlucifer@users.noreply.github.com>
2023-02-02 23:53:12 +0530

    Update 154. Find Minimum in Rotated Array II.cpp

--- binary search/Logic Building (same as Advanced)/154. Find Minimum in Rotated Array II.cpp
+++ binary search/Logic Building (same as Advanced)/154. Find Minimum in Rotated Array II.cpp
@@ -1,3 +1,22 @@
 // Leetcode HARD  --  Same as Part 1 but with duplicates existing in it!      |     But as usual, my brain is too small to figure things out. Nvm.. moving on!
 
-
+int findMin(vector<int>& nums) {
+        int lo = 0;
+        int hi = nums.size() - 1;
+        int mid = 0;
+        
+        while(lo < hi) {
+            mid = lo + (hi - lo) / 2;
+            
+            if (nums[mid] > nums[hi]) {
+                lo = mid + 1;
+            }
+            else if (nums[mid] < nums[hi]) {
+                hi = mid;
+            }
+            else { // when num[mid] and num[hi] are same: GOLDEN TICKET!
+                hi--;   // just remove it from end!
+            }
+        }
+        return nums[lo];
+    }
