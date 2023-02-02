bb5a733d7c2cc4323e1e053ccd137b31769d52c6
Mukul Malik <85161950+serzaxlucifer@users.noreply.github.com>
2023-02-02 22:11:09 +0530

    Create 153. Minimum in Sorted Array.cpp

---
+++ binary search/Logic Building (same as Advanced)/153. Minimum in Sorted Array.cpp
@@ -0,0 +1,80 @@
+// my first implementation:
+
+int findMin(vector<int>& nums) {
+        int size = nums.size()-1;
+        if(size == 1)                         // handled specially since this causes error if follows general algorithm
+        {
+            return min(nums[0], nums[1]);
+        }
+        int end = nums.size()-1;
+        int start = 0;
+
+        int mid = (start + end) >> 1;
+
+        while(start <= end)
+        {
+            if(nums[mid] <= nums[size])
+            {
+                // this part ain't in rotated. Need to go to its beginning.
+                if(mid == 0)
+                {
+                    return nums[0];
+                }
+                if(nums[mid] < nums[mid-1])
+                {
+                    return nums[mid];
+                }
+                if(nums[mid] >= nums[mid-1])
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+            }
+            else
+            {
+                start = mid + 1;
+            }
+            mid = (start + end) >> 1;
+        }
+
+        return nums[0];
+    }
+
+// Optimization: Logic of Binary Search remains same but we try to form better if-else conditions to process the searching. It comes down to this:
+
+int findMin(vector<int>& nums) {
+        int size = nums.size()-1;
+        int end = size;
+        int minEle = INT_MAX;
+        int start = 0;
+
+        int mid;
+
+        while(start <= end)
+        {
+            if(nums[start] < nums[end])
+            {
+                // current sequence is sorted w/o rotation.
+                if(minEle > nums[start])
+                {
+                    minEle = nums[start];
+                }
+                break;
+            }
+            mid = (start + end) >> 1;
+            if(minEle > nums[mid]) minEle = nums[mid];
+            if(nums[mid] < nums[start])
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+        }
+
+        return minEle;
+    }
