26732fcfcb08d99ae8ac14a2dbd4e9c4dc3263db
Mukul Malik <85161950+serzaxlucifer@users.noreply.github.com>
2023-02-04 03:28:32 +0530

    Update 209. Minimum Subarray.cpp

--- arrays/Advanced Sliding Windows/209. Minimum Subarray.cpp
+++ arrays/Advanced Sliding Windows/209. Minimum Subarray.cpp
@@ -28,3 +28,27 @@ int minSubArrayLen(int target, vector<int>& nums)       // Beats 99.88% solution
 
 // O(nlogn) Solution:
 
+int minSubArrayLen(int target, vector<int>& nums) 
+    {
+        // Binary Search Solution:
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+
+        int size = nums.size();
+        vector<int> prefix(size+1, 0);
+        int minLen = INT_MAX;
+
+        for(int i = 0; i < size; i++)
+        prefix[i+1] = prefix[i] + nums[i];
+
+        // From the sum array -- apply Binary Search.
+        for(int i = size; i >= 0 && prefix[i] >= target; i--)
+        {
+            int j = upper_bound(prefix.begin(), prefix.end(), prefix[i] - target) - prefix.begin();        // length!
+            minLen = (minLen < (i-j+1)) ? minLen : i-j+1;
+        } 
+
+        return (minLen == INT_MAX) ? 0 : minLen;
+
+    }
