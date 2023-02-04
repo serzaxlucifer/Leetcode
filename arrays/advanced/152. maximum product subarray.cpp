3e577c944f49e7e771b21b37ff59e5be079cc82b
Mukul Malik <85161950+serzaxlucifer@users.noreply.github.com>
2023-02-04 23:52:16 +0530

    Update 152. maximum product subarray.cpp

--- arrays/advanced/152. maximum product subarray.cpp
+++ arrays/advanced/152. maximum product subarray.cpp
@@ -1,26 +1,72 @@
-// an amazing solution:
+// Initial Approach : O(n)
+
+/*
+        The thing here is that there could either be odd or even number of negative signs. So in case of even, a single traversal will give you the result.
+        But if there are odd number of those fucking signs. We require traversal from the end as well. If there are no zeros, then it is assured that the max
+        subarray will either begin from beginning or from end.
+*/
 
 int maxProduct(vector<int>& nums) {
-        int prod=1;
-        int maxi=INT_MIN;
-        for(int i : nums){
-            prod=prod*i;
-            maxi=max(maxi,prod);
-            if(prod==0){
-                prod=1;
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        
+        int result = INT_MIN;
+        int size = nums.size();
+        int productSoFar = 1;
+
+        for(int i = 0; i < size; i++)
+        {
+            productSoFar *= nums[i];
+            result = (result < productSoFar) ? productSoFar : result;
+            if(productSoFar == 0)
+            {
+                productSoFar = 1;
             }
         }
-        prod=1;
-        reverse(nums.begin(),nums.end());
-        for(int i : nums){
-            prod=prod*i;
-            maxi=max(maxi,prod);
-            if(prod==0){
-                prod=1;
+
+        productSoFar = 1;
+
+        for(int i = size-1; i >= 0; i--)                
+        {
+            productSoFar *= nums[i];
+            result = (result < productSoFar) ? productSoFar : result;
+            if(productSoFar == 0)
+            {
+                productSoFar = 1;
             }
         }
-        return maxi;
+
+        return result;
+        
     }
+
+// Let's try to optimize: What do you think might be the optimization here? Well, its simple. Can we combine these two loops into one?? Certainly! Do both stuff in one!
+
+
+
+// an amazing solution:
+int maxProduct(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        
+        int result = INT_MIN;
+        int size = nums.size();
+        int product = 1;
+        int productEnd = 1;
+
+        for(int i = 0; i < size; i++)
+        {
+            product*= nums[i];
+            productEnd*= nums[size-i-1];
+            result = max({result, product, productEnd});
+            if(product==0) product = 1;
+            if(productEnd==0) productEnd = 1;
+        }
+
+        return result;
+        
+    }
+
 
 //
 
