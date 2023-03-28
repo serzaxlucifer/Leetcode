ad95386ecb76a7a95aa5b8ee235d1a56f9937a0b
Mukul Malik <85161950+serzaxlucifer@users.noreply.github.com>
2023-03-28 22:38:36 +0530

    Create 21. Merge 2 sorted lists.cpp

---
+++ Linked Lists/Logic Building/21. Merge 2 sorted lists.cpp
@@ -0,0 +1,31 @@
+ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) 
+    {
+        ListNode* dummy = new ListNode();   // add to it.
+        ListNode* temp = dummy;
+
+        while(list1 && list2)
+        {
+            if(list1->val < list2->val)
+            {
+                temp->next = list1;
+                list1 = list1->next;
+            }
+            else
+            {
+                temp->next = list2;
+                list2 = list2->next;
+            }
+            temp = temp->next;
+        }
+
+        if(list1)
+        {
+            temp->next = list1;
+        }
+        if(list2)
+        {
+            temp->next = list2;
+        }
+
+        return dummy->next;
+    }
