4ae1826882d77e7cd430200e2c76e0eef2a03f54
Mukul Malik <85161950+serzaxlucifer@users.noreply.github.com>
2023-08-23 02:51:22 +0530

    Update 46. Permutations.cpp

--- Backtracking/46. Permutations.cpp
+++ Backtracking/46. Permutations.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution {            // LeetCode Editorial Solution
 public:
     vector<vector<int>> permute(vector<int>& nums) 
     {
@@ -50,5 +50,39 @@ public:
         }
         
         
+    }
+};
+
+// LOC is too high in above code. Can we reduce the lines of code?
+// Another logic to backtrack and generate same output: (courtesy of Love Babbar ['CodeHelp by Love Babbar' on YouTube])
+// This is as optimized as this can get both time and space wise.
+// Code Inspiration: https://www.geeksforgeeks.org/write-a-c-program-to-print-all-permutations-of-a-given-string/
+
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) 
+    {
+        vector<vector<int>> answer;
+
+        permute_helper(nums, answer, 0, nums.size()-1);
+
+        return answer;
+    }
+
+    void permute_helper(vector<int>& nums, vector<vector<int>>& answer, int l, int r)
+    {
+        
+        if(l == r)
+        {
+            answer.push_back(nums);
+            return;
+        }
+
+        for(int i = l; i <= r; i++)
+        {
+            swap(nums[l], nums[i]);
+            permute_helper(nums, answer, l+1, r);
+            swap(nums[l], nums[i]);
+        }
     }
 };
