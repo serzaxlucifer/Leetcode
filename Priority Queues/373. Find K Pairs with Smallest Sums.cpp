9ac4f49b6ea47c7d2cb4d94b2133ddd76caf62b0
Mukul Malik <85161950+serzaxlucifer@users.noreply.github.com>
2023-08-29 20:04:28 +0530

    Update 373. Find K Pairs with Smallest Sums.cpp

--- Priority Queues/373. Find K Pairs with Smallest Sums.cpp
+++ Priority Queues/373. Find K Pairs with Smallest Sums.cpp
@@ -1,1 +1,39 @@
-// 
+vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) 
+    {
+        vector<vector<int>> answer;
+
+        int m = nums1.size(), n = nums2.size();
+
+        set<pair<int, int>> visited;
+
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> minHeap;
+
+        minHeap.push({nums1[0] + nums2[0], {0, 0}});
+        visited.insert({0, 0});
+
+        while (k-- && !minHeap.empty()) 
+        {
+            auto top = minHeap.top();
+
+            minHeap.pop();
+
+            int i = top.second.first;
+            int j = top.second.second;
+
+            answer.push_back({nums1[i], nums2[j]});
+
+            if (i + 1 < m && visited.find({i + 1, j}) == visited.end()) 
+            {
+                minHeap.push({nums1[i + 1] + nums2[j], {i + 1, j}});
+                visited.insert({i + 1, j});
+            }
+
+            if (j + 1 < n && visited.find({i, j + 1}) == visited.end()) 
+            {
+                minHeap.push({nums1[i] + nums2[j + 1], {i, j + 1}});
+                visited.insert({i, j + 1});
+            }
+        }
+
+        return answer;
+    }
