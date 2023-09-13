41a594a851e09df4b10efaa804f7afe1da161722
Mukul Malik <85161950+serzaxlucifer@users.noreply.github.com>
2023-09-14 00:56:44 +0530

    Create 1092. Shortest Common Supersequence [IMPORTANT].cpp

---
+++ Dynamic Programming/1092. Shortest Common Supersequence [IMPORTANT].cpp
@@ -0,0 +1,12 @@
+// This is a good question to develop skills in DP on Strings!
+// Let's get this DONE~!
+
+// Our first approach, let's take it as decomposition to already known questions and then using their result to derive our result.
+// See, what can we use?! LCS, LPS? Something should work!
+
+// YES, We can compute the LCS string and then EASILY form the supersequence!
+// Let's do that before we try to attempt anything else. Both memoized and tabulated ofc ;)
+
+
+
+
