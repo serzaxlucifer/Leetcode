641bf1658fc488e014ef2da0db251630e8a6f3b2
Mukul Malik <85161950+serzaxlucifer@users.noreply.github.com>
2023-12-24 00:37:52 +0530

    Update 684. Redundant Connection.cpp

--- Graphs/684. Redundant Connection.cpp
+++ Graphs/684. Redundant Connection.cpp
@@ -67,6 +67,57 @@ public:
         }
     }
 };
+// DSU:
+class Solution {
+public:
+    int findParent(int x, vector<int> &parent) 
+    {
+        if (parent[x] == x)
+            return x;
+        return parent[x] = findParent(parent[x], parent);               // path compression
+    }
+    bool Union(int p1, int p2, vector<int> &parent, vector<int> &rank) 
+    {
+    
+        p1 = findParent(p1, parent);
+        p2 = findParent(p2, parent);
+
+        if (p1 == p2)
+            return false;
+        if (rank[p1] > rank[p2]) 
+        {
+            parent[p2] = p1;
+        }   
+        else if(rank[p2]>rank[p1])
+        {
+            parent[p1] = p2;
+        }
+        else
+        {
+            rank[p1]++;
+            parent[p2]=p1;
+        }
+        return true;
+    }
+
+    vector<int> findRedundantConnection(vector<vector<int>> &edges) 
+    {
+        int n = edges.size();
+        vector<int> parent(n + 1);
+        
+        for (int i = 0; i <= n; i++)
+            parent[i] = i;
+
+        vector<int> rank(n + 1, 0);
+
+        for (int i = 0; i < n; i++) 
+        {
+            if (!Union(edges[i][0], edges[i][1], parent, rank)) // !Union-> means they are already connected
+                return edges[i];
+        }
+        return {};
+    }
+};
 
 
 
