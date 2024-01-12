ce1c79c7aadf666bd6cfd93a552e3e9747035c54
Mukul Malik <85161950+serzaxlucifer@users.noreply.github.com>
2024-01-13 03:43:05 +0530

    Create 1091. Shortest Path in Binary Matrix.cpp

---
+++ Graphs/1091. Shortest Path in Binary Matrix.cpp
@@ -0,0 +1,93 @@
+int shortestPathBinaryMatrix(vector<vector<int>>& grid) 
+    {
+        if(grid[0][0])
+        {
+            return -1;
+        }
+
+        vector<vector<int>> distance(grid.size(), vector<int>(grid[0].size(), 1e9));
+        queue<pair<int, pair<int, int>>> BFS;
+        distance[0][0] = 0;
+        BFS.push({0, {0, 0}});
+        int m = grid.size();
+        int n = grid[0].size();
+
+        if(m == 1 && n == 1)
+        {
+            return 1;
+        }
+
+        int dx[8] = {1, -1, 0, 0, 1, -1, 1, -1};
+        int dy[8] = {1, -1, 1, -1, 0, 0, -1, 1};
+
+        while(!BFS.empty())
+        {
+            auto it = (BFS.front());
+            int dist = it.first;
+            int x = it.second.first;
+            int y = it.second.second;
+
+            BFS.pop();
+
+            for(int i = 0; i < 8; i++)
+            {
+                int nx = x + dx[i], ny = y + dy[i];
+
+                if(nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == 0 && dist + 1 < distance[nx][ny])
+                {
+                    distance[nx][ny] = dist + 1;
+                    if(nx == m-1 && ny == n-1)
+                    {
+                        return distance[nx][ny]+1;
+                    }
+                    BFS.push({1 + dist, {nx, ny}});
+                }
+            }
+        }
+
+
+        return -1;
+
+    }
+
+// Best Approach:
+
+int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        if (grid[0][0] != 0 or grid.back().back() != 0) return -1;
+
+        int n = grid.size();
+        if (n == 1) return 1;
+
+        int res = 1;
+        static int q[100000]; 
+        int start = 0, end = 1; 
+        q[0] = (0 << 16) | 0;
+        grid[0][0] = 1;
+
+        while (start < end) {
+            int newEnd = end; // Track where the next segment ends
+
+            for (int i = start; i < end; ++i) {
+                int x = q[i] >> 16; // Extract x
+                int y = q[i] & 0xFFFF; // Extract y
+
+                if (x == n - 1 && y == n - 1) return res;
+
+                for (int j = 0; j < 8; ++j) {
+                    int x_new = x + dirs[j][0];
+                    int y_new = y + dirs[j][1];
+
+                    if (x_new >= 0 && x_new < n && y_new >= 0 && y_new < n && grid[x_new][y_new] == 0) {
+                        grid[x_new][y_new] = 1;
+                        q[newEnd++] = (x_new << 16) | y_new;
+                    }
+                }
+            }
+
+            start = end;
+            end = newEnd;
+            ++res;
+        }
+
+        return -1;
+    }
