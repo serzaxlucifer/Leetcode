74e97d16faee8acc48c8a56da66be28a67c0eae0
Mukul Malik <85161950+serzaxlucifer@users.noreply.github.com>
2024-01-13 03:58:21 +0530

    Update 1091. Shortest Path in Binary Matrix.cpp

--- Graphs/1091. Shortest Path in Binary Matrix.cpp
+++ Graphs/1091. Shortest Path in Binary Matrix.cpp
@@ -50,6 +50,62 @@ int shortestPathBinaryMatrix(vector<vector<int>>& grid)
 
     }
 
+// Even better
+
+class Solution {
+public:
+    
+    bool isValid(vector<vector<int>>& grid, int i, int j, int n, vector<vector<bool>>& visited){
+        
+        return (i>=0 and i<n and j>=0 and j<n and grid[i][j]==0 and !visited[i][j]);
+        
+    }
+    
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        
+        int n = grid.size();
+        vector<vector<bool>> visited(n, vector<bool> (n, false));
+        queue<pair<int, int>> q;
+        int ans = 0;
+        int nodesPushed;
+        
+        if(grid[0][0] == 0){
+            q.push({0, 0});
+            visited[0][0] = true;
+        }
+                
+        while(!q.empty()){
+            
+            nodesPushed = q.size();
+            ans++;
+            
+            for(int cnt = 0; cnt < nodesPushed; cnt++){
+                
+                pair<int, int> frontNode = q.front();
+                q.pop();
+            
+                int i = frontNode.first, j = frontNode.second;
+
+                if(i==n-1 and j==n-1) return ans;
+
+                for(int k = i - 1; k <= i + 1 ; k++){
+                    for(int l = j - 1; l <= j + 1; l++){
+                        if(isValid(grid, k, l, n, visited)){
+                            q.push({k, l});
+                            visited[k][l] = true;
+                        }
+                    }
+                }                
+                
+            }            
+            
+        }
+        
+        return -1;
+        
+    }
+};
+
 // Best Approach:
 
 int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
@@ -59,7 +115,7 @@ int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         if (n == 1) return 1;
 
         int res = 1;
-        static int q[100000]; 
+        int q[100000]; 
         int start = 0, end = 1; 
         q[0] = (0 << 16) | 0;
         grid[0][0] = 1;
