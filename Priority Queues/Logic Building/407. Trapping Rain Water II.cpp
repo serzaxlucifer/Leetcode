2eed10acbfb567299382a3c656915c877f539e6a
Mukul Malik <85161950+serzaxlucifer@users.noreply.github.com>
2024-02-06 00:26:22 +0530

    Update 407. Trapping Rain Water II.cpp

--- Priority Queues/Logic Building/407. Trapping Rain Water II.cpp
+++ Priority Queues/Logic Building/407. Trapping Rain Water II.cpp
@@ -1,4 +1,40 @@
-    int trapRainWater(vector<vector<int>>& heightMap) 
+/*
+        If you are thinking of converting the classic trapping rain water to this,
+    then it might not work as expected. Lets say, for each position, you compute the
+    horizontal blocking height and the vertical blocking height. Now that you know the
+    blocking heights for all 4 dirs, you might think the volume computation from min of these
+    blocking heights will give the answer, but it won't give optimal answer.
+    
+    Reason being, that we are not considering the case when any of the connecting horizontal or
+    vertical blocks might have a way to drain the water.
+    
+    4 4 4 4
+    4 3 3 4
+    4 4 3 4,
+    Here as we can see even though horizontal and vertical blocking height logic seems to give ans = 1 (at (1, 1) block), in reality
+    it is 0. The block at (1, 2) will spill the water via (2, 2) and won't be able to hold any water, so the block at
+    (1, 1) won't be able to hold any water as well no matter what.
+    
+    Now how do we find the solution ?
+    What if we are able to find a way to approach a block via its min height blocking cell and then just check if the height of min block
+    cell is greater than curr. If it is greater, then curr cell can hold water, this is because we were able to reach the current cell via
+    the min height blocking cell and that means remaining blocking cells are all greater in height than this path cell. So if this path cell
+    is greater than current, then all other blocking cells are greater as well.
+    
+    Now to actually do this in practice, we make use of min heap. We add the initial border cells to heap. Then slowly start reaching the inner
+    cells via the shortest height boundary cells, while keeping track of tallest cell boundary in the path.
+    Each time we reach an inner cell, there are 2 possibilities:
+    1. Inner cell is greater, then we add this to heap as well. This can act as a blocking height for more inner cells.
+    2. Inner cell is smaller, then we know that this inner call can hold water = path_cell - inner_cell. Add this to heap as well
+    
+    While adding new elements to heap, make sure to add the max height of that path, since that is the actual boundary in that path.
+    
+    TC: O(MNlog(MN))
+    SC: O(MN)
+
+*/
+
+int trapRainWater(vector<vector<int>>& heightMap) 
     {
         std::ios::sync_with_stdio(false);
         std::cin.tie(nullptr);
