0d02b8a2de0f1bb22154f44fe58a953a4b970a86
Mukul Malik <85161950+serzaxlucifer@users.noreply.github.com>
2024-02-09 02:36:05 +0530

    Create 1871. Jump Game VII.cpp

---
+++ Dynamic Programming/Advanced/1871. Jump Game VII.cpp
@@ -0,0 +1,62 @@
+// A recursive construct looks like this:
+
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) 
+    {
+        return aux(s, minJump, maxJump, 0);
+    }
+
+    bool aux(string s, int minJump, int maxJump, int start)
+    {
+        if(start >= s.length()-1)
+        {
+            return true;
+        }
+
+        bool reach = false;
+
+        for(int i = min(start + maxJump, (int)s.length()-1); i >= start + minJump; i--)
+        {
+            if(s[i] == '0')
+            {      
+                reach = aux(s, minJump, maxJump, i);
+            }
+            if(reach)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+// It is efficient in time BUT... memory limits are exceeded due to call stack on a specific example.
+// Solution is to use an iterative solution. Can we convert to an iterative solution?
+
+bool canReach(string s, int mi, int ma) 
+    {
+        if(s.back()=='1')
+            return false; 
+
+        vector<bool>dp(s.size());
+        dp[0] = true;
+        int count = 0;
+
+        for(int i = 1; i<s.size(); i++){
+            if(i>ma)
+            {
+                count-=dp[i-ma-1]; //Moving sliding window forward by decrementing the count by the dp value of the index that is no longer in our window
+            }
+            if(i>=mi)
+            {
+                count+=dp[i-mi];
+            }
+            if(count>0&&s[i]=='0')
+                dp[i] = true;
+            else 
+                dp[i] = false;
+        }
+        return dp.back();
+    }
