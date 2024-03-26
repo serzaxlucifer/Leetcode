8252925111928e768c8daa7dfba4b81d71326f90
Mukul Malik <85161950+serzaxlucifer@users.noreply.github.com>
2024-03-26 18:36:56 +0530

    Create 1472. Design Browser History.cpp

---
+++ Linked Lists/1472. Design Browser History.cpp
@@ -0,0 +1,37 @@
+class BrowserHistory {
+    vector<string> history;
+    int position = 0;
+
+public:
+    BrowserHistory(string homepage) 
+    {
+        history.push_back(homepage);
+    }
+    
+    void visit(string url) 
+    {
+       position++;
+       history.resize(position);
+       history.push_back(url); 
+    }
+    
+    string back(int steps) 
+    {
+        position = max(0, position - steps);
+        return history[position];
+    }
+    
+    string forward(int steps) 
+    {
+        position = min((int)history.size() - 1, position + steps);
+        return history[position];
+    }
+};
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory* obj = new BrowserHistory(homepage);
+ * obj->visit(url);
+ * string param_2 = obj->back(steps);
+ * string param_3 = obj->forward(steps);
+ */
