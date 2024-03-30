f088ba1ae00552cbe1610819b9da0c4a991eca36
Mukul Malik <85161950+serzaxlucifer@users.noreply.github.com>
2024-03-30 07:47:02 +0530

    Create 432. All O-one Data Structure.cpp

---
+++ Linked Lists/432. All O-one Data Structure.cpp
@@ -0,0 +1,123 @@
+class BucketNode
+{
+    public:
+    int count;
+    unordered_set<string> keySet;
+    BucketNode *next;
+    BucketNode *prev;
+
+    BucketNode(int cnt) 
+    {
+        count = cnt;
+    }
+};
+
+class AllOne {
+public:
+    BucketNode *head, *tail;
+    unordered_map<int, BucketNode*> countBucketMap;
+    unordered_map<string, int> keyCountMap;
+
+    AllOne() {
+        head = new BucketNode(INT_MIN);
+        tail = new BucketNode(INT_MAX);
+        head->next = tail;
+        tail->prev = head;
+    }
+    
+    void inc(string key) 
+    {
+        if(keyCountMap.find(key) != keyCountMap.end())
+        {
+            changeKey(key, 1);
+        }
+        else
+        {
+            keyCountMap[key] = 1;
+            if (head->next->count != 1) 
+            {
+                insertBucket(new BucketNode(1), head);
+            }
+            head->next->keySet.insert(key);
+            countBucketMap[1] = head->next;
+        }
+    }
+    
+    void dec(string key) 
+    {
+        if(keyCountMap.find(key) != keyCountMap.end())
+        {
+            int count = keyCountMap[key];
+            if(count == 1)
+            {
+                keyCountMap.erase(key);
+                removeKey(countBucketMap[count], key);
+            }
+            else
+            {
+                changeKey(key, -1);
+            }
+        }
+    }
+    
+    string getMaxKey() 
+    {
+        return tail->prev == head ? "" : *(tail->prev->keySet.begin());
+    }
+    
+    string getMinKey() 
+    {
+        return head->next == tail ? "" : *(head->next->keySet.begin());
+    }
+
+    void changeKey(string key, int offset)
+    {
+        // offset is either 1 or -1
+
+        int count = keyCountMap[key];
+        keyCountMap[key] = count + offset;
+
+        BucketNode* curBucket = countBucketMap[count];
+        BucketNode* newBucket;
+
+        if(countBucketMap.find(count+offset) != countBucketMap.end())
+        {
+            newBucket = countBucketMap[count+offset];
+        }
+        else
+        {
+            newBucket = new BucketNode(count+offset);
+            countBucketMap[count+offset] = newBucket;
+            insertBucket(newBucket, offset == 1 ? curBucket : curBucket->prev);
+        }
+
+        newBucket->keySet.insert(key);
+        removeKey(curBucket, key);
+    }
+
+    void removeKey(BucketNode* bucket, string key)
+    {
+        bucket->keySet.erase(key);
+        if(bucket->keySet.size() == 0)
+        {
+            removeBucket(bucket);
+            countBucketMap.erase(bucket->count);
+        }
+    }
+
+    void insertBucket(BucketNode* newBucket, BucketNode* afterThis)
+    {
+        newBucket->prev = afterThis;
+        newBucket->next = afterThis->next;
+        afterThis->next->prev = newBucket;
+        afterThis->next = newBucket;
+    }
+
+    void removeBucket(BucketNode* bucket)
+    {
+        bucket->prev->next = bucket->next;
+        bucket->next->prev = bucket->prev;
+        bucket->next = NULL;
+        bucket->prev = NULL;
+    }
+};
